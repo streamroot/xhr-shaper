271e5139523f94586b76be643b4d419870cf0f48
Stephan Hesse <tchakabam@gmail.com>
2016-07-29 14:21:48 +0200

    fix some things around how we export to env

--- index.js
+++ index.js
@@ -4,25 +4,16 @@ var BaseXHR = require('./src/base-xhr');
 var objectMirrors = require('./src/object-mirrors');
 var XMLHttpRequestShim = require('./src/shim');
 
-// Export all our stuff via module if we are one
-if (typeof module !== 'undefined') {
-    module.exports = {
-        BaseXHR: BaseXHR,
-        XMLHttpRequest: XMLHttpRequestShim,
-        useGlobal: useGlobal,
-        objectMirrors: objectMirrors
-    };
-}
-
 function useGlobal() {
     // Shim window/global XHR
     var global = window || global;
-    if (typeof global !== 'undefined') {
-        // Overload native window constructor
-        global.XMLHttpRequest = XMLHttpRequestShim;
-    }
+    // Overload native window constructor
+    global.XMLHttpRequest = XMLHttpRequestShim;
 }
 
-
-
-
+module.exports = {
+    BaseXHR: BaseXHR,
+    XMLHttpRequest: XMLHttpRequestShim,
+    useGlobal: useGlobal,
+    objectMirrors: objectMirrors
+};
